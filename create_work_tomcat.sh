2452b244b8b29217a56c3141b734cdf2f99f6657
Viet Shell <vietstack@gmail.com>
2014-05-09 14:58:24 +0700

    multi work

---
+++ create_work_tomcat.sh
@@ -0,0 +1,151 @@
+#!/bin/sh
+
+#  create_work_tomcat.sh
+#  
+#
+#  Created by HSP SI Viet Nam on 5/9/14.
+#
+clear
+
+tenshell=`echo $0`
+echo "Script Chi Dung Rieng Cho Da Nang"
+number_work=`ls /var/www/html | wc -l`
+echo $number_work
+
+#configure port
+if [ "$number_work" -le 9 ]; then
+sv_port="8"$number_work"05"
+sv_connecter="8"$number_work"80"
+sv_connecport="8"$number_work"09"
+fi
+
+#configure port
+if [ "$number_work" -gt 9 ]; then
+sv_port="8"$number_work"5"
+sv_connecter="8"$number_work"8"
+sv_connecport="8"$number_work"9"
+fi
+
+echo "Dien ten sub domain ma ban muon tao:"
+echo "Luu Y; Chi dien sub khong dien domain."
+echo "Domain mac dinh: Yton.vn"
+read -p"Sub Domain: " sdm
+
+#check subdomain rong
+if [ "$sdm" = "" ]; then
+    echo "sub doamin khong duoc la rong"
+    echo " Dien lai"
+    sleep 3
+    sh $tenshell
+    exit 1
+fi
+
+#check trung subdomain
+check_sdm=`ls /etc/httpd/conf.d/ | grep $sdm.yton.vn.conf`
+if [ "$check_sdm" != "" ]; then
+clear
+    echo "Sub Domain "$sdm".yton.vn.conf da ton tai"
+    echo "Vui long Dien lai Sub Domain khac"
+    echo "Nhap Lai"
+    sh $tenshell
+    exit 1
+fi
+
+#create subdomain
+mkdir -p /var/www/html/$sdm
+cd
+cp vwork.tar.gz /var/www/html/$sdm/vwork.tar.gz
+cd /var/www/html/$sdm
+tar -xvf vwork.tar.gz
+rm -rf vwork.tar.gz
+cd
+chown -R hsp. /var/www/html/$sdm
+chmod -R 755 /var/www/html/$sdm
+chmod -R +rx /var/www/html
+#change port conflig
+cat > /var/www/html/$sdm/conf/server.xml << eof
+<?xml version='1.0' encoding='utf-8'?>
+<Server port="$sv_port" shutdown="SHUTDOWN">
+<Listener className="org.apache.catalina.core.AprLifecycleListener" SSLEngine="on" />
+<Listener className="org.apache.catalina.core.JasperListener" />
+<Listener className="org.apache.catalina.core.JreMemoryLeakPreventionListener" />
+<Listener className="org.apache.catalina.mbeans.GlobalResourcesLifecycleListener" />
+<Listener className="org.apache.catalina.core.ThreadLocalLeakPreventionListener" />
+<GlobalNamingResources>
+<Resource name="UserDatabase" auth="Container"
+type="org.apache.catalina.UserDatabase"
+description="User database that can be updated and saved"
+factory="org.apache.catalina.users.MemoryUserDatabaseFactory"
+pathname="conf/tomcat-users.xml" />
+</GlobalNamingResources>
+<Service name="Catalina">
+<Connector port="$sv_connecter" protocol="HTTP/1.1"
+connectionTimeout="20000"
+redirectPort="8443" />
+<Connector port="$sv_connecport" protocol="AJP/1.3" redirectPort="8443" />
+<Engine name="Catalina" defaultHost="localhost">
+<Realm className="org.apache.catalina.realm.LockOutRealm">
+<Realm className="org.apache.catalina.realm.UserDatabaseRealm"
+resourceName="UserDatabase"/>
+</Realm>
+
+<Host name="localhost"  appBase="webapps"
+unpackWARs="true" autoDeploy="true">
+<Valve className="org.apache.catalina.valves.AccessLogValve" directory="logs"
+prefix="localhost_access_log." suffix=".txt"
+pattern="%h %l %u %t &quot;%r&quot; %s %b" />
+
+</Host>
+</Engine>
+</Service>
+</Server>
+eof
+
+
+
+#create service
+cat > /usr/bin/start_$sdm << eof
+export CATALINA_HOME=/var/www/html/hsp_web
+export CATALINA_BASE=/var/www/html/$sdm
+cd \$CATALINA_HOME/bin
+./startup.sh
+eof
+chmod +x /usr/bin/start_$sdm
+
+cat > /usr/bin/stop_$sdm << eof
+export CATALINA_HOME=/var/www/html/hsp_web
+export CATALINA_BASE=/var/www/html/$sdm
+cd \$CATALINA_HOME/bin
+./shutdown.sh
+eof
+chmod +x /usr/bin/stop_$sdm
+
+
+#create shell restart service
+cat > /usr/bin/reset_$sdm << rsthspvn
+sv=\`lsof -i | grep $sdm\`
+kill -9 \$sv
+/usr/bin/start_$sdm start
+clear
+echo "Restart $sdm success"
+rsthspvn
+
+chmod +x /usr/bin/reset_$sdm
+
+
+#Create vhost hspvn
+cat > /etc/httpd/conf.d/$sdm.yton.vn.conf << vhhspvn
+<VirtualHost *:80>
+ServerName $sdm.yton.vn
+#    ServerAlias mid.yton.vn
+ProxyPreserveHost on
+ProxyRequests     off
+ProxyPass / ajp://localhost:$sv_connecport/
+ProxyPassReverse / ajp://localhost:$sv_connecport/
+</VirtualHost>
+vhhspvn
+
+/etc/init.d/httpd reload
+/usr/bin/start_$sdm start
+clear
+echo " Install Success Full"
---
+++ tomcat_multiwork.sh
@@ -0,0 +1,207 @@
+#!/bin/sh
+
+#  tomcat_multiwork.sh
+#  
+#
+#  Created by HSP SI Viet Nam on 5/9/14.
+#
+#Check Install packet
+clear
+sed -i 's/SELINUX=enforing/SELINUX=disabled/g' /etc/selinux/config
+hsp_dir="/var/www/html/hsp_web"
+ro_dir="/root"
+checkjava=`rpm -qa | grep java-1.7`
+if [ "$checkjava" = "" ]; then
+yum -y install java-1.7.0-openjdk
+fi
+
+#check httpd
+checkhttpd=`ls /etc/ | grep httpd`
+if [ "$checkhttpd" = "" ]; then
+yum -y install httpd httpd-*
+fi
+
+#Install Other Packet
+yum -y install mlocate wget man
+
+#download packet apache
+wget http://mirror.nexcess.net/apache/tomcat/tomcat-7/v7.0.53/bin/apache-tomcat-7.0.53.tar.gz
+tar -xvf apache-tomcat-7.0.53.tar.gz
+mv apache-tomcat-7.0.53 $hsp_dir
+cd $hsp_dir
+tar -cvf vwork.tar.gz conf bin logs temp webapps work
+mv $hsp_dir/vwork.tar.gz $ro_dir/vwork.tar.gz
+
+sed -i 's/\#ServerName www.example.com:80/ServerName hsp-vn.com:80/g' /etc/httpd/conf/httpd.conf
+sed -i 's/\#NameVirtualHost/NameVirtualHost/g' /etc/httpd/conf/httpd.conf
+sed -i 's/ServerTokens OS/ServerTokens Prod/g' /etc/httpd/conf/httpd.conf
+sed -i 's/KeepAlive Off/KeepAlive On/g' /etc/httpd/conf/httpd.conf
+sed -i 's/AllowOverride None/AllowOverride All'
+
+#Restart service httpd
+/etc/init.d/httpd restart
+chkconfig httpd on
+
+#adduser hsp
+useradd -d $hsp_dir hsp
+chown -R hsp. $hsp_dir
+chmod -R 755 $hsp_dir
+
+#Setting Iptables
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+
+#clean all rules iptables
+iptables -t nat -P PREROUTING ACCEPT
+iptables -t nat -P OUTPUT ACCEPT
+iptables -t nat -P POSTROUTING ACCEPT
+iptables -t mangle -P PREROUTING ACCEPT
+iptables -t mangle -P INPUT ACCEPT
+iptables -t mangle -P FORWARD ACCEPT
+iptables -t mangle -P OUTPUT ACCEPT
+iptables -t mangle -P POSTROUTING ACCEPT
+
+#Dell all
+iptables -F
+iptables -t nat -F
+iptables -t mangle -F
+
+iptables -X
+iptables -t nat -X
+iptables -t mangle -X
+
+#Zero all packets and counters
+iptables -Z
+iptables -t nat -Z
+iptables -t mangle -Z
+
+
+#Set rule iptables
+iptables -A INPUT -p tcp -m tcp --dport 80 -j LOG
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p icmp -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -m state --state NEW -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -m state --state NEW -p tcp --dport 22 -j ACCEPT
+iptables -A OUTPUT -m state --state NEW -p tcp --dport 80 -j ACCEPT
+iptables -A OUTPUT -m state --state NEW -p tcp --dport 22 -j ACCEPT
+iptables -A OUTPUT -m state --state NEW -p tcp --dport 1521 -j ACCEPT
+iptables -A OUTPUT -m state --state NEW -p tcp --dport 443 -j ACCEPT
+/etc/init.d/iptables save
+/etc/init.d/iptables restart
+chkconfig iptables on
+
+
+#Create service hsp
+echo "CATALINA_HOME=/var/www/html/hsp_web" >> /etc/bashrc
+cat > /etc/init.d/hspvn << hspservice
+#!/bin/bash
+
+# Apache Tomcat7: Start/Stop Chuong Trinh
+#
+# chkconfig: - 90 10
+
+
+. /etc/init.d/functions
+. /etc/sysconfig/network
+export CATALINA_BASE=$hsp_dir
+CATALINA_BASE=$hsp_dir
+CATALINA_HOME=$hsp_dir
+TOMCAT_USER=hsp
+LOCKFILE=/var/lock/subsys/hsp
+
+RETVAL=0
+start(){
+echo "Khoi Dong Chuong Trinh: "
+su - \$TOMCAT_USER -c "\$CATALINA_HOME/bin/startup.sh"
+RETVAL=\$?
+echo
+[ \$RETVAL -eq 0 ] && touch \$LOCKFILE
+return \$RETVAL
+}
+
+stop(){
+echo "Ngat Chuong Trinh: "
+\$CATALINA_HOME/bin/shutdown.sh
+RETVAL=\$?
+echo
+[ \$RETVAL -eq 0 ] && rm -f \$LOCKFILE
+return \$RETVAL
+}
+
+case "\$1" in
+start)
+start
+;;
+stop)
+stop
+;;
+restart)
+stop
+start
+;;
+status)
+status tomcat
+;;
+*)
+echo \$"Usage: \$0 {start|stop|restart|status}"
+exit 1
+;;
+esac
+exit \$?
+hspservice
+
+#End Create service
+#configure service
+chmod +x /etc/init.d/hspvn
+chkconfig --add hspvn
+chkconfig hspvn on
+/etc/init.d/hspvn start
+
+
+#create shell restart service
+cat > /usr/bin/hspvnrestart << rsthspvn
+sv=`lsof -i | grep hsp`
+kill -9 \$sv
+/etc/init.d/hspvn start
+clear
+echo "Restart HSPVN success"
+rsthspvn
+chmod +x /usr/bin/hspvnrestart
+
+
+#Create vhost
+cat > /etc/httpd/conf.d/1.conf << defaulthttp
+<VirtualHost *:80>
+ServerName 172.16.6.68
+#    ServerAlias mid.yton.vn
+ProxyPreserveHost on
+ProxyRequests     off
+ProxyPass / ajp://localhost:8009/
+ProxyPassReverse / ajp://localhost:8009/
+</VirtualHost>
+defaulthttp
+
+#Create vhost hspvn
+cat > /etc/httpd/conf.d/hspvn.conf << vhhspvn
+<VirtualHost *:80>
+ServerName hspvn.yton.vn
+#    ServerAlias mid.yton.vn
+ProxyPreserveHost on
+ProxyRequests     off
+ProxyPass / ajp://localhost:8009/
+ProxyPassReverse / ajp://localhost:8009/
+</VirtualHost>
+vhhspvn
+
+/etc/init.d/httpd reload
+
+clear
+echo " Install Success Full"
+yum -y update
+clear
+echo " Reboot Server"
+echo " Please wait....."
+sleep 5
+reboot
